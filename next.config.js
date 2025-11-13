2e47995fb57527cba1f64e8eec9c58c34e309af3
Adaste Enterprises <adasteenterprises@gmail.com>
2025-11-13 13:49:01 +0300

    Save local changes before rebase

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {},
+    },
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
+
